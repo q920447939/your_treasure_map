85992ec97b270fe96009ca3fda9181de1203f3b4
l <a@a.com>
2025-03-07 14:56:03 +0800

    test: testing pre-commit hook

--- markdown.py
+++ markdown.py
@@ -3,15 +3,28 @@ import os
 # 指定要读取的文件夹路径
 folder_path = "."
 
-def generate_markdown_toc(folder_path):
+def generate_markdown_content():
+    header = """# your_treasure_map
+This is a collection of all kinds of software engineering records, you will always find what you need
 
+"""
+    return header + "\n\n\n\n"
+
+def generate_markdown_toc(folder_path):
     def traverse_folder(path, level=0):
         toc = []
         folder_name = os.path.basename(path)
         has_md_files = False
 
-        for item in sorted(os.listdir(path)):
+        # 获取目录下所有文件和文件夹，并按名称排序
+        items = sorted(os.listdir(path))
+        
+        for item in items:
             item_path = os.path.join(path, item)
+            # 忽略 .git 目录和 README.md
+            if ".git" in item_path or item == "README.md":
+                continue
+                
             if os.path.isdir(item_path):
                 subtree = traverse_folder(item_path, level + 1)
                 if subtree:
@@ -36,6 +49,7 @@ def generate_markdown_toc(folder_path):
 
     return "\n".join(traverse_folder(folder_path))
 
-# 调用函数并打印生成的目录
-markdown_toc = generate_markdown_toc(folder_path)
-print(markdown_toc)
+# 生成内容并写入 README.md
+content = generate_markdown_content() + generate_markdown_toc(folder_path)
+with open("README.md", "w", encoding="utf-8") as f:
+    f.write(content)
